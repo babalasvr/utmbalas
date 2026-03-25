88d938ce1d15cf287c5dd2ca30a61d1206d32a68
babalasvr <ghsbatista@gmail.com>
2026-03-25 00:31:50 -0300

    fix: expor DASHBOARD_PASSWORD no next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['node:sqlite'],
   },
+  env: {
+    DASHBOARD_PASSWORD: process.env.DASHBOARD_PASSWORD || 'utmbalas123',
+  },
 };
 
 module.exports = nextConfig;
